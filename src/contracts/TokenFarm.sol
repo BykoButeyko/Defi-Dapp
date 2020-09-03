fd8d674f8f2edd0b1cf049d139f7d5d9a8f609ec
Byko <bykocrypto@gmail.com>
2020-09-03 23:35:51 +0200

    Issuing tokens (Reward for staking) function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,6 +5,7 @@ import './DaiToken.sol';
 
 contract TokenFarm {
     string public name = "Clorofeel Token Farm";
+    address public owner;
     ClorofeelToken public clorofeelToken;
     DaiToken public daiToken;
 
@@ -16,10 +17,14 @@ contract TokenFarm {
     constructor(ClorofeelToken _clorofeelToken, DaiToken _daiToken) public {
         clorofeelToken = _clorofeelToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     // 1. Stakes tokens (Deposit)
     function stakeTokens(uint _amount) public {
+        //Require amount greater than 0
+        require(_amount > 0, "amount cannot be 0");
+
         // Transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
 
@@ -40,4 +45,16 @@ contract TokenFarm {
     //2. Unstaking tokens (Withdraw)
 
     //3. Issuing Tokens (Reward for staking)
+    function issueToken() public {
+        require(msg.sender == owner, "caller must be the owner");
+
+        //Issue tokens to all stakers
+        for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0) {
+                clorofeelToken.transfer(recipient, balance);
+            }
+        }
+    }
 }
