13e77814afbcfef50580c6f264f9c94cc2d52937
Byko <bykocrypto@gmail.com>
2020-09-04 01:42:10 +0200

    Load all data from the Blockchain & setup Frontend App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react'
+import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import ClorofeelToken from '../abis/ClorofeelToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
 import Navbar from './Navbar'
 import './App.css'
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0]})
+
+    const networkId = await web3.eth.net.getId()
+
+    //Load DaiToken
+    const daiTokenData = DaiToken.networks[networkId]
+    if(daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+      this.setState({ daiToken })
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString()})
+      console.log({ balance: daiTokenBalance})
+    } else {
+      window.alert('DaiToken contract not deployed to detected network.')
+    }
+
+    //Load ClorofeelToken
+    const clorofeelTokenData = ClorofeelToken.networks[networkId]
+    if(clorofeelTokenData) {
+      const clorofeelToken = new web3.eth.Contract(ClorofeelToken.abi, clorofeelTokenData.address)
+      this.setState({ clorofeelToken })
+      let clorofeelTokenBalance = await clorofeelToken.methods.balanceOf(this.state.account).call()
+      this.setState({ clorofeelTokenBalance: clorofeelTokenBalance.toString()})
+      console.log({ balance: clorofeelTokenBalance})
+    } else {
+      window.alert('ClorofeelToken contract not deployed to detected network.')
+    }
+
+    //Load TokenFarm
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if(tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+      this.setState({ tokenFarm })
+      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString()})
+    } else {
+      window.alert('TokenFarm contract not deployed to detected network.')
+    }
+
+    this.setState({ loading: false})
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider MetaMask!')
+    }
+  }
+
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+      daiToken: {},
+      clorofeelToken: {},
+      tokenFarm: {},
+      daiTokenBalance: '0',
+      clorofeelTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
     }
   }
 
@@ -20,7 +94,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="http://www.dappuniversity.com"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
